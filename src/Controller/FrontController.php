df17f5cdfa1e32238d54f01d8a9739a5b95fe7c3
krystianlewandowski <Krylew5399>
2021-10-19 22:50:48 +0200

    add to front data

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -10,14 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/front", name="front")
+     * @Route("/", name="front")
      */
     public function index(): Response
     {
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
-        dump();
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'users' => $users,
         ]);
     }
 }
